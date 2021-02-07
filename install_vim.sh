0fd5ed97ca4fd4d5db5fa8589400597114a8f947
Mateusz Kalkowski <kalkowski.mateusz.dev@gmail.com>
2021-02-07 16:09:41 +0100

    Update install_vim.sh

--- install_vim.sh
+++ install_vim.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 
-sudo apt install libncurses5-dev libgtk2.0-dev libatk1.0-dev \
-libcairo2-dev libx11-dev libxpm-dev libxt-dev python2.7-dev  \
-python3-dev ruby-dev lua5.2 liblua5.2-dev libperl-dev git
+sudo apt-get remove --purge vim vim-runtime vim-gnome vim-tiny vim-gui-common
+sudo rm -rf /usr/local/share/vim /usr/bin/vim
 
-sudo apt remove vim vim-runtime gvim vim-tiny vim-common vim-gui-common vim-nox
+sudo apt-get install -y liblua5.1-dev luajit libluajit-5.1 python-dev ruby-dev libperl-dev libncurses5-dev libatk1.0-dev libx11-dev libxpm-dev libxt-dev python3.8-dev
 
-cd ~
-git clone https://github.com/vim/vim.git
-cd vim
-./configure --with-features=huge \
-            --enable-multibyte \
-            --enable-rubyinterp=yes \
-            --enable-python3interp=yes \
-            --with-python3-config-dir=$(python3-config --configdir) \
-            --enable-perlinterp=yes \
-            --enable-luainterp=yes \
-            --enable-gui=gtk2 \
-            --enable-cscope \
-            --prefix=/usr/local
+git clone https://github.com/vim/vim ~/vimtemp
+cd ~/vimtemp
+git pull && git fetch
 
+./configure \
+--with-features=huge \
+--enable-multibyte \
+--enable-rubyinterp=yes \
+--enable-perlinterp=yes \
+--enable-luainterp=yes \
+--enable-cscope \
+--prefix=/usr/local \
+--enable-python3interp=yes \
+--with-python3-config-dir=/usr/lib/python3.8/config-3.8m-x86_64-linux-gnu \
+--enable-gui=auto \
+--with-x \
+--enable-fontset \
+--enable-largefile \
+--disable-netbeans \
+--with-compiledby="Mateusz Kalkowski" \
+--enable-fail-if-missing
 
+make && sudo make install
 
-make VIMRUNTIMEDIR=/usr/local/share/vim/vim82
-
-sudo apt install checkinstall
-cd ~/vim
-sudo checkinstall
+rm -rf ~/vimtemp
 
 sudo update-alternatives --install /usr/bin/editor editor /usr/local/bin/vim 1
 sudo update-alternatives --set editor /usr/local/bin/vim
